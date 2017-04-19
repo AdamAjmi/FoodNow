0b8594a6a12ab60bdd677d77f30a6b7f5927cc31
Adam Ajmi <adam.ajmi@gmail.com>
2017-04-19 07:23:04 +0200

    Removed unused service file.

--- food_now/api/services/GmapsService.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Adam Ajmi 12.04.2017.
- */
-
-var gmapsApiKey = "AIzaSyDB2SxMScx_UhuA8A_tudtOo1Onu-1H-VU";
-var url = "https://maps.googleapis.com/maps/api/js";
-var request = require('request');
-
-
